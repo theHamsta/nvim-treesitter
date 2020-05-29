fb2bb209b1c2dc4c6f8de4c18aba99746199a3b9
Stephan Seitz <stephan.seitz@fau.de>
2020-05-29 19:01:45 +0200

    Start markdown/highlights.scm

---
+++ queries/markdown/highlights.scm
@@ -0,0 +1,10 @@
+(list_marker) @operator
+(html_block (text)) @inject.html
+(code_fence_content) @constant
+(indented_code_block) @constant
+(link_destination) @function
+(link_text) @string
+(atx_heading) @type
+(emphasis) @builtin
+(strong_emphasis) @type
+;image (image_description (text)) (link_destination (text))
